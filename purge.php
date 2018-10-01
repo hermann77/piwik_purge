aad3e4081bee87aa911342d0bbab4824607b1592
Hermann Schwarz <tux1977@gmx.de>
2018-10-01 18:52:31 +0200

    debugging output removed.

--- purge.php
+++ purge.php
@@ -43,11 +43,9 @@ try {
     print 'OPTIMIZE executed' . PHP_EOL;
 
 
-
     /**
      * REMOVE ARCHIVE TABLES
      */
-    
     for($year = 2000; $year <= $purge_till_year; ++$year) {
 
        for($month = 1; $month <= 12; ++$month) {
@@ -55,24 +53,18 @@ try {
             $month = str_pad($month, 2, '0', STR_PAD_LEFT);
 
             $sql = 'DROP TABLE IF EXISTS piwik_archive_blob_' . $year . '_' . $month;
-            print 'SQL (blob): ' . $sql . PHP_EOL;
             $stmt = $pdo->prepare($sql);
             $stmt->execute();
             $stmt->closeCursor();
             print 'TABLE piwik_archive_blob_' . $year . '_' . $month . ' dropped' . PHP_EOL;
 
             $sql = 'DROP TABLE IF EXISTS piwik_archive_numeric_' . $year . '_' . $month;
-            print 'SQL (archive): ' . $sql . PHP_EOL;
             $stmt = $pdo->prepare($sql);
             $stmt->execute();
             $stmt->closeCursor();
             print 'TABLE piwik_archive_numeric_' . $year . '_' . $month . ' dropped' . PHP_EOL;
        }
     }
-
-    
-
-
 
 }
 catch(PDOException $e) {
